bc52f6fffb1f0757ee72d145cf3c845c05821bf7
fnaticIT <namanbalai06@gmail.com>
2021-10-02 00:10:46 +0530

    added leetcode problem rotate image

---
+++ C++/48-Rotate_image.cpp
@@ -0,0 +1,31 @@
+/*
+Leetcode - medium difficulty
+48 - Rotate image
+
+Method :-
+
+  first swap m[i][j] with m[j][i], then reverse up to down 
+
+  1 2 3     3 2 1     3 6 9
+  4 5 6  => 6 5 4  => 2 5 8
+  7 8 9     9 8 7     1 4 7
+
+*/
+
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& m) {
+
+        int n = m.size();
+        
+        //swapping
+        for(int i=0; i<n; i++)
+            for(int j=0; j<i; j++)
+                swap(m[i][j], m[j][i]);
+        
+        //reverse operation
+        for(int i=0; i<n; i++)
+            reverse(m[i].begin(), m[i].end());
+    }
+};
--- C++/README.md
+++ C++/README.md
@@ -11,6 +11,7 @@
 15 : 3Sum<br>
 21 : Merge two sorted lists<br>
 31 : Next Permutation<br>
+48 : Rotate Image<br>
 55 : Jump Game<br>
 121 : Best Time To Buy And Sell Stock<br>
 198 : House Robber<br>
