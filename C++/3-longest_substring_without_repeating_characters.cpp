89f82faf70dee48d99b93354be7cab605f5ea6d1
Arvinder Singh Kandola <askandola07@gmail.com>
2021-10-01 10:41:39 +0530

    added comments

--- C++/3-longest_substring_without_repeating_characters.cpp
+++ C++/3-longest_substring_without_repeating_characters.cpp
@@ -1,17 +1,35 @@
-//Problem link : https://leetcode.com/problems/longest-substring-without-repeating-characters/
+/* 
+3. Longest Substring Without Repeating Characters
+
+Given a string s, find the length of the longest substring without repeating characters.
+
+ */
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int st = 0, n = s.length(), ans = 0;
+        //hash map for storing frequency of characters of string s
         unordered_map<char, int> ump;
         for(int en=0; en<n; en++) {
             if(ump[s[en]]!=0 && ump[s[en]]>=st) {
-                st  =ump[s[en]];
+                //s[en] is already present in current substring
+                //update start of current substring to next character
+                st = ump[s[en]];
             }
+            //store the 1-based index of current character in hash map
             ump[s[en]] = en+1;
             ans = max(ans, en-st+1);
         }
         return ans;
     }
 };
+
+/* 
+Sample input : "abcabcbb"
+Sample output : 3
+
+Time Complexity : O(n)
+Space Complexity : O(n)
+n = length of string
+ */
