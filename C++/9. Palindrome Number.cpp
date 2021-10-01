25bcc09e2c3f097a652a6817eda2e37fa454ddf4
Piyush228 <45069778+Piyush228@users.noreply.github.com>
2021-10-02 00:48:11 +0530

    Create 9. Palindrome Number.cpp

---
+++ C++/9. Palindrome Number.cpp
@@ -0,0 +1,42 @@
+// PROBLEM STATEMENT
+
+/*Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+Example 4:
+
+Input: x = -101
+Output: false   */
+
+// CODE 
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0|| (x!=0 &&x%10==0)) return false;
+        int sum=0;
+        while(x>sum)
+        {
+            sum = sum*10+x%10;
+            x = x/10;
+        }
+        return (x==sum)||(x==sum/10);
+    }
+};
