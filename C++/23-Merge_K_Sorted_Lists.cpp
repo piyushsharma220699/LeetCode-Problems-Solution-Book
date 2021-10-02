435949c4e74cf2b7332f5b16407e625a331316c4
Sagar Jaswal <80746702+sj9102001@users.noreply.github.com>
2021-10-02 22:08:25 +0530

    Update 23-Merge_K_Sorted_Lists.cpp

--- C++/23-Merge_K_Sorted_Lists.cpp
+++ C++/23-Merge_K_Sorted_Lists.cpp
@@ -1,13 +1,11 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+/*Link Of Question : https://leetcode.com/problems/merge-k-sorted-lists/
+23. Merge k Sorted Lists
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+*/
+//Solution : 
+
 class Solution
 {
 public:
@@ -99,3 +97,19 @@ public:
         return third;
     }
 };
+
+/*
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+
+Time Comeplexity : O(n*log(k))
+Space Complexity : O(n)
+*/
