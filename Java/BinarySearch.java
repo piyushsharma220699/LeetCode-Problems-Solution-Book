46b60ac29b1eab16ba50300b6e0a92deb72a52cf
Soumyajoy Das <70829126+soumyajoy01@users.noreply.github.com>
2021-10-04 11:23:26 +0530

    Binary Search Solution Added in Java

--- Java/BinarySearch.java
+++ Java/BinarySearch.java
@@ -9,15 +9,12 @@
         System.out.println(ans);
     }
 
-    // return the index
-    // return -1 if it does not exist
     static int binarySearch(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
 
         while(start <= end) {
-            // find the middle element
-//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+        
             int mid = start + (end - start) / 2;
 
             if (target < arr[mid]) {
@@ -25,7 +22,6 @@
             } else if (target > arr[mid]) {
                 start = mid + 1;
             } else {
-                // ans found
                 return mid;
             }
         }
